1937bc8541026a3b14b2a51dd84e94085defe18d
ketrin996 <bloomingkate789@gmail.com>
2017-09-28 13:13:15 +0300

    Task 2: sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
         print(args);
+        sort(args);
+        print(args);
     }
 
     public static void print(String[] args) {
         for(String i:args) {
             System.out.println(i);
+        }
+    }
+
+    public static void sort(String[] args) {
+        for (int i = 0; i <= args.length - 1; i++) {
+            for (int j = 1; j < args.length; j++) {
+                int comp = args[j].compareTo(args[i]);
+                if (comp < 0) {
+                    String temp = args[i];
+                    args[i]=args[j];
+                    args[j] = temp;
+                }
+            }
         }
     }
 
